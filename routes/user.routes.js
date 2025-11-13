e78a8fcb302bdbe7a49b7a4510850312d7d51a08
Mohamed Adham <madham2004@gmail.com>
2025-11-14 01:36:05 +0200

    added updateMe controller and updated user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const checkLoginAuth = require('../middleware/check-login-auth');
-const getAllUsers = require('../controllers/user/getall');
-const getUserById = require('../controllers/user/getone');
-const updateUser = require('../controllers/user/update');
-const deleteUser = require('../controllers/user/delete');
+const userController = require('../controllers/user/user.controller');
+const user = require('../model/user');
 
-router.get('/', checkLoginAuth, getAllUsers);
-router.get('/:id', checkLoginAuth, getUserById);
-router.patch('/:id', checkLoginAuth, updateUser);
-router.delete('/:id', checkLoginAuth, deleteUser);
+router.get('/', checkLoginAuth, userController.getAllUsers);
+router.patch('/me', checkLoginAuth, userController.updateMe);
+router.get('/:id', checkLoginAuth, userController.getOneUser);
+router.patch('/:id', checkLoginAuth, userController.updateUser);
+router.delete('/:id', checkLoginAuth, userController.deleteUser);
 
 module.exports = router;
